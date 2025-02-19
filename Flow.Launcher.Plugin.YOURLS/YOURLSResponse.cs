294c30902b711849af4385a723a312da32caccc5
Bennett <no-reply@bennettsh.de>
2025-02-19 15:54:41 +0100

    fix(net): parsing for error codes

--- Flow.Launcher.Plugin.YOURLS/YOURLSResponse.cs
+++ Flow.Launcher.Plugin.YOURLS/YOURLSResponse.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Flow.Launcher.Plugin.YOURLS
 {
+    internal class NumberOrStringConverter : JsonConverter<int>
+    {
+        public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.TokenType switch
+            {
+                JsonTokenType.Number => reader.GetInt32(),
+                JsonTokenType.String when int.TryParse(reader.GetString(), out int result) => result,
+                _ => throw new JsonException("Invalid number format")
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
+        {
+            writer.WriteNumberValue(value);
+        }
+    }
+
     internal class YOURLSResponse
     {
         public string status { get; set; }
         public string code { get; set; }
         public string message { get; set; }
         public string errorCode { get; set; }
+        [JsonConverter(typeof(NumberOrStringConverter))]
         public string statusCode { get; set; }
         public string title { get; set; }
         public string shorturl { get; set; }
